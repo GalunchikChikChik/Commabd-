95ad2aa6ab132ffdc96b5e08a99a066693ecb58e
Nikita Zhernovenkov <nikzhernovenkov778@gmail.com>
2020-12-30 03:59:29 +0300

    Adding getaChar, ClientInputScreen::setClient, ClientList::insertClient methods

--- LORDMETOD.cpp
+++ LORDMETOD.cpp
@@ -1,3 +1,52 @@
 #include <iostream> 
 #include <string> 
 #include "LORD.h" 
+
+char getaChar()
+{
+	char ch;
+	while (true)
+	{
+		ch = getchar();
+		if (ch != '\n')
+		{
+			break;
+		}
+	}
+	getchar();
+	return ch;
+}
+
+///////////метод класса ClientInputScreen////////////////// 
+
+void ClientInputScreen::setClient()
+{
+	cout Ђ "¬ведите наименование клиента: " Ђ endl;
+	getaLine(tName);
+	cout Ђ "¬ведите номер склада: " Ђ endl;
+	cin ї warehousenumber;
+	cin.ignore(80, '\n');
+	Client* ptrClient = new Client(tName, warehousenumber);
+	ptrClientList->insertClient(ptrClient);
+}
+
+void ClientList::insertClient(Client* ptrT)
+{
+	bool exists = false;
+	for (iter = setPtrsClients.begin(); iter != setPtrsClients.end(); iter++)
+	{
+		if ((*iter)->getWareHouse() == ptrT->getWareHouse())
+		{
+			exists = true;
+			break;
+		}
+	}
+	if (!exists)
+		setPtrsClients.push_back(ptrT);
+	else
+	{
+
+		delete ptrT;
+		cout Ђ "\n—клад забронирован ";
+	}
+}
